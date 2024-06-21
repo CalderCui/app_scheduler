acaa7b8204045e9d1dc650a3fdf58a2ddacff4a8
CalderCui <CalderCui@gmail.com>
2024-06-21 15:10:21 -0700

    update

--- app_getter.py
+++ app_getter.py
@@ -1,71 +1,3 @@
-# import requests
-# import time
-# from selenium import webdriver
-# from selenium.webdriver.common.by import By
-# from selenium.webdriver.common.keys import Keys
-# from selenium.webdriver.support.ui import WebDriverWait
-# from selenium.webdriver.support import expected_conditions as EC
-# from selenium.webdriver.chrome.service import Service as ChromeService
-# from webdriver_manager.chrome import ChromeDriverManager
-# import time
-#
-#
-# def app_getter():
-#
-#     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
-#     login_url = 'https://prenotami.esteri.it/Home/'
-#     try:
-#         driver.get(login_url)
-#         wait = WebDriverWait(driver, 10)
-#         email_field = wait.until(EC.presence_of_element_located((By.ID, 'login-email')))
-#         pw_field = wait.until(EC.presence_of_element_located((By.ID, 'login-password')))
-#
-#         email_field.send_keys('npbq26@gmail.com')
-#         pw_field.send_keys('Quincee7')
-#
-#
-#
-#         forward_button = wait.until(EC.presence_of_element_located((By.XPATH, '//button[@type="submit" and @class="button primary g-recaptcha"]')))
-#         forward_button.click()
-#
-#         # log_in_title = driver.title
-#
-#         while driver.title == 'Unavailable':
-#             time.sleep(600)
-#             driver.back()
-#             wait = WebDriverWait(driver, 10)
-#             email_field = wait.until(EC.presence_of_element_located((By.ID, 'login-email')))
-#             pw_field = wait.until(EC.presence_of_element_located((By.ID, 'login-password')))
-#             email_field.send_keys('npbq26@gmail.com')
-#             pw_field.send_keys('Quincee7')
-#             forward_button = wait.until(EC.presence_of_element_located(
-#                 (By.XPATH, '//button[@type="submit" and @class="button primary g-recaptcha"]')))
-#             forward_button.click()
-#
-#         service_field = wait.until(EC.presence_of_element_located((By.ID, 'advanced')))
-#
-#         service_field.click()
-#         while True:
-#             time.sleep(25)
-#             book_button = driver.find_element(By.XPATH, '//a[@href="/Services/Booking/4996"]')
-#
-#             book_button.click()
-#
-#             time.sleep(5)
-#             current_url = driver.current_url
-#             if "/Services" in current_url:
-#
-#                 driver.refresh()
-#             else:
-#                 print('go register')
-#                 break
-#     except ValueError as e:
-#         print({e})
-#
-#
-# if __name__=='__main__':
-#     app_getter()
-
 import time
 from selenium import webdriver
 from selenium.webdriver.common.by import By
@@ -73,7 +5,9 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service as ChromeService
 from webdriver_manager.chrome import ChromeDriverManager
-from selenium.common.exceptions import WebDriverException, TimeoutException
+from selenium.common.exceptions import WebDriverException, TimeoutException, NoSuchElementException
+
+
 
 def app_getter():
     driver = webdriver.Chrome(service=ChromeService(ChromeDriverManager().install()))
@@ -108,7 +42,7 @@ def app_getter():
         service_field.click()
 
         while True:
-            time.sleep(25)
+            time.sleep(5)
             current_url = driver.current_url
             try:
                 if "/Services" in current_url and "/Booking/4996" not in current_url:
@@ -127,8 +61,20 @@ def app_getter():
                     driver.refresh()
                     WebDriverWait(driver, 10).until(EC.staleness_of(old_page))
                 elif "Booking/4996" in current_url:
-                    driver.back()
-                    time.sleep(5)
+                    try:
+                        error_message = driver.find_element(By.XPATH,
+                                                            '//span[@jsselect="heading" and @jsvalues=".innerHTML:msg" and @jstcache="9"]')
+                        if error_message and "This site can’t be reached" in error_message.text:
+                            print('Site can’t be reached, retrying...')
+                            driver.back()
+                            time.sleep(5)
+                        else:
+                            print('Go register')
+                            break
+                    except NoSuchElementException:
+                        # No error message, assume the page is valid and pause
+                        print('Page reached, pausing...')
+                        break
                 else:
                     print('Go register')
                     break
